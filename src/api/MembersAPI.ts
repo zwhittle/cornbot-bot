93d7585b9dd1336c2e2e167d891b5272824e8c64
Zach Whittle <zach@zachwhittle.com>
2023-06-11 23:31:51 -0500

    add daily birthday check back

--- src/api/MembersAPI.ts
+++ src/api/MembersAPI.ts
@@ -1,6 +1,7 @@
 import { Collection, GuildMember } from 'discord.js'
 import { Member } from '../interfaces/Member'
 import { CornbotAPI } from './CornbotAPI'
+import { startOfToday } from 'date-fns'
 
 export class MembersAPI extends CornbotAPI<Member> {
   constructor() {
@@ -14,6 +15,15 @@ export class MembersAPI extends CornbotAPI<Member> {
   async incrementCorns(id: string) {
     const member = await this.one(id)
     await this.update(id, { corns: ++member.corns })
+  }
+
+  async todaysBirthdays() {
+    const members = await this.all()
+    return members.filter(
+      member =>
+        member.birthdayDay === startOfToday().getDate() &&
+        member.birthdayMonth === startOfToday().getMonth() + 1
+    )
   }
 }
 
--- src/events/ready.ts
+++ src/events/ready.ts
@@ -2,6 +2,7 @@ import { Client } from 'discord.js'
 import { GuildsAPI } from '../api/GuildsAPI'
 import { MembersAPI } from '../api/MembersAPI'
 import { AnalyticsAPI } from '../api/AnalyticsAPI'
+import { CronJob } from 'cron'
 
 export const ready = async (BOT: Client) => {
   const guildsApi = new GuildsAPI()
@@ -26,4 +27,27 @@ export const ready = async (BOT: Client) => {
       membersApi.sync(members)
     })
   })
+
+  const dailyBirthdayCheck = new CronJob('00 00 09 * * *', async () => {
+    // This should run every day at 09:00:00
+    const birthdayMembers = await membersApi.todaysBirthdays()
+    let guildsWithBirthday = []
+    birthdayMembers.map(member => {
+      if (guildsWithBirthday.filter(element => element.id == member.guildId).length == 0) {
+        guildsWithBirthday.push(member.guildId)
+      }
+    })
+
+    guildsWithBirthday.map(async guildId => {
+      const guildBirthdayMembers = birthdayMembers.filter(member => member.guildId === guildId)
+      if (guildBirthdayMembers.length >= 1) {
+        const guild = await BOT.guilds.fetch(guildId)
+        const birthdayMembersString = birthdayMembers.map(member => `<@${member.id}>`).join(', ')
+        const output = `Happy Birthday to ${birthdayMembersString}`
+        guild.systemChannel.send(output)
+      }
+    })
+  })
+
+  dailyBirthdayCheck.start() // start
 }
