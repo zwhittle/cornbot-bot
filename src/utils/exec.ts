288699e59803ba4db47ace9980ccf27bc33fc78e
Zach Whittle <zach@zachwhittle.com>
2024-07-15 05:13:45 -0500

    remove spring tour exec command

--- src/utils/exec.ts
+++ src/utils/exec.ts
@@ -45,41 +45,6 @@ export async function launchVolitionXTour(client: Client) {
 
 }
 
-export async function launchSpring2024Tour(client: Client) {
-  const cornserv = await client.guilds.fetch('847637234613878824')
-  const springTour = tourData.find(tour => tour.key === 'spring2024')
-
-  const events: GuildScheduledEventCreateOptions[] = []
-
-  springTour.dates.map(async show => {
-    const venue = show.venue
-    const address = venue.address
-
-    let description = 'Ontario, Canada Tour - Spring 2024\n\n'
-    description += `${venue.name}\n`
-
-    const startTime = show.time ? `${show.date}T${show.time}` : `${show.date}T20:00:00-04:00`
-
-    const showEvent: GuildScheduledEventCreateOptions = {
-      name: show.name,
-      scheduledStartTime: startTime,
-      scheduledEndTime: `${show.date}T23:00:00-04:00`,
-      privacyLevel: GuildScheduledEventPrivacyLevel.GuildOnly,
-      entityType: 3,
-      description: description,
-      entityMetadata: { location: venue.name },
-      image: springTour.poster,
-    }
-
-    events.push(showEvent)
-  })
-
-  events.map(
-    async event =>
-      await cornserv.scheduledEvents.create(event).then(e => console.log(`${e.name} created`))
-  )
-}
-
 export async function launchHalloween2023Tour(client: Client) {
   const cornserv = await client.guilds.fetch('847637234613878824')
   const halloweenTour = tourData.find(tour => tour.key === 'halloween2023')
